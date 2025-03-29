8b666313a93aacfbfdc396e19b5ab29774751317
Riskiana Wulan <riskiana.wulan@kuelap.io>
2025-03-29 10:36:18 +0700

    wordlength rule implementation

--- src/main/java/org/riskiana/rules/WordLengthRule.java
+++ src/main/java/org/riskiana/rules/WordLengthRule.java
@@ -3,21 +3,31 @@ package org.riskiana.rules;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.riskiana.Constants;
 import org.riskiana.Rule;
 
 @Slf4j(topic = Constants.LOGGER_NAME)
 public class WordLengthRule implements Rule {
-  private static int LONG_WORD_LENGTH = 5;
-  private Set<String> uniqueLongWords = new HashSet<>();
+  private static final int LONG_WORD_LENGTH = 5;
+  private final Set<String> uniqueLongWords = new HashSet<>();
 
   @Override
   public void apply(List<String> words) {
     log.info("Applying word length rule");
+   final var longWords = words.stream()
+        .filter(word -> word.length() > LONG_WORD_LENGTH)
+        .collect(Collectors.toSet());
+    uniqueLongWords.addAll(longWords);
+    log.info("uniqueLongWords with length > {}: {}",LONG_WORD_LENGTH,  uniqueLongWords);
 
   }
 
+
+  public Set<String> getUniqueLongWords() {
+    return uniqueLongWords;
+  }
 
 
 }
--- src/test/java/org/riskiana/RuleProcessorTest.java
+++ src/test/java/org/riskiana/RuleProcessorTest.java
@@ -1,6 +1,7 @@
 package org.riskiana;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,10 +10,11 @@ import org.riskiana.rules.UppercaseWordRule;
 import org.riskiana.rules.WordLengthRule;
 
 public class RuleProcessorTest {
-  private UppercaseWordRule uppercaseWordRule;
-  private WordLengthRule wordLengthRule;
+
   static String file1 = "src/test/resources/file1.txt";
   static String file2 = "src/test/resources/file2.html";
+  private UppercaseWordRule uppercaseWordRule;
+  private WordLengthRule wordLengthRule;
 
   @BeforeEach
   public void setUp() {
@@ -30,6 +32,21 @@ public class RuleProcessorTest {
     List<String> words = List.of("HELLO", "world", "HELLO", "world");
     uppercaseWordRule.apply(words);
     assertEquals(2, uppercaseWordRule.getCount());
+  }
+
+  @Test
+  public void testWordLengthRule() {
+    List<String> words = List.of("Indonesia", "Belgia", "USA", "Canada");
+    wordLengthRule.apply(words);
+    final var expected = List.of("Indonesia", "Belgia", "Canada");
+    assertTrue(expected.containsAll(wordLengthRule.getUniqueLongWords()));
+  }
+
+  @Test
+  public void testEmptyText() {
+    List<String> words = List.of();
+    uppercaseWordRule.apply(words);
+    assertEquals(0, uppercaseWordRule.getCount());
   }
 
 }
